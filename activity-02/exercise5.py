31cef314ca665dac779fb48ea8d1139b23e91aa9
Matías Puente <Mat.psandoval@gmail.com>
2020-11-29 04:04:14 -0300

    ex7 started

---
+++ activity-02/exercise5.py
@@ -0,0 +1,17 @@
+n = int(input("Ingrese un numero:"))
+
+
+# Solution A
+# x = 0
+# a = ""
+
+# while n > x:
+#     a = a + "*"
+#     x += 1
+
+# x = 0
+
+# while n > x:
+#     print(a)
+#     x +=1
+
---
+++ activity-07/exercise7.py
@@ -0,0 +1,10 @@
+import os
+
+file_dir = os.path.dirname(__file__)
+file_signal = open(os.path.join(file_dir, "signal.txt"))
+file_network = open(os.path.join(file_dir, "networks.txt"))
+
+signal = file_signal.readlines()
+network = file_network.readlines()
+
+print(network)
